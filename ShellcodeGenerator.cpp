417bab5f8b352cca29a27c1726bfd591555b024d
vomicz <t.vomacka@seznam.cz>
2023-07-16 07:23:12 -0700

    wchar_t refactoring

--- ShellcodeGenerator.cpp
+++ ShellcodeGenerator.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
-    ShellExecute(NULL, L"open", L"calc.exe", NULL, NULL, SW_SHOW);
-    return EXIT_SUCCESS;
+	wchar_t openParam[] = { 'o', 'p', 'e', 'n', 0 };
+	wchar_t calcParam[] = { 'c', 'a', 'l', 'c', '.', 'e', 'x', 'e', 0 };
+	ShellExecute(NULL, openParam, calcParam, NULL, NULL, SW_SHOW);
+	return EXIT_SUCCESS;
 }
